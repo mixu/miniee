af6cfa2c03313dc492408db9c14186954cc47d08
Mikito Takada <firstname.surname@gmail.com>
2012-03-02 16:31:39 -0800

    Update tests to mocha, add makefile, add browserbuild support, add dist for browser

--- dist/miniee.js
+++ dist/miniee.js
@@ -1,3 +1,4 @@
+(function () { var global = this; function require(p){ var path = require.resolve(p) , mod = require.modules[path] || (global && global[path]); if (!mod) throw new Error('failed to require "' + p + '"'); if (!mod.exports) { mod.exports = {}; mod.call(mod.exports, mod, mod.exports, require.relative(path), global); } return mod.exports;}require.modules = {};require.resolve = function(path){ var orig = path , reg = path + '.js' , index = path + '/index.js'; return require.modules[reg] && reg || require.modules[index] && index || orig;};require.register = function(path, fn){ require.modules[path] = fn;};require.relative = function(parent) { return function(p){ if ('.' != p.charAt(0)) return require(p); var path = parent.split('/') , segs = p.split('/'); path.pop(); for (var i = 0; i < segs.length; i++) { var seg = segs[i]; if ('..' == seg) path.pop(); else if ('.' != seg) path.push(seg); } return require(path.join('/')); };};require.register("miniee.js", function(module, exports, require, global){
 /**
  * MiniEE is a client and server side library for routing events.
  * Main difference from EventEmitter is that callbacks can be specified using RegExps.
@@ -18,7 +19,7 @@ MiniEventEmitter.prototype.each = function(obj, iterator, context) {
     }
   } else {
     for (var key in obj) {
-      if (obj.hasOwnProperty(key)) {
+      if (_.has(obj, key)) {
         if (iterator.call(context, obj[key], key, obj) === breaker) return;
       }
     }
@@ -138,3 +139,6 @@ MiniEventEmitter.mixin	= function(destObject){
 
 module.exports	= MiniEventEmitter;
 
+
+});miniee = require('miniee');
+})();
--- test/miniee.test.js
+++ test/miniee.test.js
@@ -1,95 +1,103 @@
-var Mee = require('../miniee.js');
+var assert = require('assert'),
+    MiniEventEmitter = require('../miniee.js');
 
 // set and emit an event using a string
 
 var ee;
 exports['given a new miniee'] = {
-  setUp: function(done) {
-    ee = new Mee();
-    done();
-  },
-  tearDown: function(done) {
+
+  beforeEach: function(done) {
+    ee = new MiniEventEmitter();
     done();
   },
 
-  'should be able to set a listener using a string': function(test) {
-    ee.on('test', function(arg) { test.ok(arg === 'success'); test.done(); } );
+  afterEach: function(done) {
+    done();
+  },
+
+  'should be able to set a listener using a string': function(done) {
+    ee.on('test', function(arg) { assert.ok(arg === 'success'); done(); } );
     ee.emit('test', 'success');
   },
 
-  'should be able to set a listener using a regexp': function(test) {
-    ee.on(/test.*/, function(arg) { test.ok(arg === 'success'); test.done(); } );
+  'should be able to set a listener using a regexp': function(done) {
+    ee.on(/test.*/, function(arg) { assert.ok(arg === 'success'); done(); } );
     ee.emit('test', 'success');
   },
 
-  'a regexp listener should match varying events': function(test) {
-    test.expect(2);
+  'a regexp listener should match varying events': function(done) {
+    var assertions = 0;
     ee.on(/test.*/, function(arg) {
       if(arg === 'success') {
-        test.ok(true);
+        assert.ok(true);
+        assertions++;
       }
       if(arg == 'final') {
-        test.ok(true); test.done();
+        assert.ok(true);
+        assertions++;
+        assert.equal(2, assertions);
+        done();
       }
     });
     ee.emit('testing', 'success');
     ee.emit('test', 'final');
   },
 
-  'listeners set using once should only fire once': function(test) {
-    test.expect(1);
+  'listeners set using once should only fire once': function(done) {
+    var assertions = 0;
     ee.once(/test.*/, function(arg) {
       if(arg === 'success') {
-        test.ok(true);
+        assert.ok(true);
+        assertions++;
       }
       if(arg == 'final') {
-        test.ok(false);
+        assert.ok(false);
+        assertions++;
       }
     });
     ee.emit('testing', 'success');
     ee.emit('test', 'final');
-    test.done();
+    done();
   },
 
-  'can set multiple listeners with the same string': function(test) {
-    test.expect(2);
-    ee.on('test', function(arg) { test.ok(arg === 'success');  } );
-    ee.on('test', function(arg) { test.ok(arg === 'success'); test.done(); } );
+  'can set multiple listeners with the same string': function(done) {
+    var assertions = 0;
+    ee.on('test', function(arg) { assert.ok(arg === 'success'); assertions++; } );
+    ee.on('test', function(arg) { assert.ok(arg === 'success'); assertions++; assert.equal(2, assertions); done(); } );
     ee.emit('test', 'success');
   },
 
-  'can set multiple listeners with the same regexp': function(test) {
-    test.expect(2);
-    ee.on(/aaa.*/, function(arg) { test.ok(arg === 'success'); } );
-    ee.on(/aaa.*/, function(arg) { test.ok(arg === 'success'); test.done(); } );
+  'can set multiple listeners with the same regexp': function(done) {
+    var assertions = 0;
+    ee.on(/aaa.*/, function(arg) { assert.ok(arg === 'success'); assertions++; } );
+    ee.on(/aaa.*/, function(arg) { assert.ok(arg === 'success'); assertions++; assert.equal(2, assertions); done(); } );
     ee.emit('aaaa', 'success');
   },
 
-  'can pass an arbitrary number of arguments on events': function(test) {
-    test.expect(8);
+  'can pass an arbitrary number of arguments on events': function(done) {
     ee.on('test', function(a, b, c, d, e, f, g, h) {
-      test.equal(a, 'as');
-      test.equal(b, 'easy');
-      test.equal(c, 'as');
-      test.equal(d, '1');
-      test.equal(e, '2');
-      test.equal(f, '3');
-      test.equal(g, 'it');
-      test.equal(h, 'works');
-      test.done();
+      assert.equal(a, 'as');
+      assert.equal(b, 'easy');
+      assert.equal(c, 'as');
+      assert.equal(d, '1');
+      assert.equal(e, '2');
+      assert.equal(f, '3');
+      assert.equal(g, 'it');
+      assert.equal(h, 'works');
+      done();
     });
     ee.emit('test', 'as', 'easy', 'as', '1', '2', '3', 'it', 'works');
   },
 
-  'setting more than one once() will still trigger all events': function(test) {
-    test.expect(3);
-    ee.once(/aaa.*/, function(arg) { test.ok(arg === 'success'); console.log('1');  } );
-    ee.on(/aaa.*/, function(arg) { test.ok(arg === 'success'); console.log('2'); } );
-    ee.once(/aaa.*/, function(arg) { test.ok(arg === 'success'); console.log('3'); test.done(); } );
+  'setting more than one once() will still trigger all events': function(done) {
+    var assertions = 0;
+    ee.once(/aaa.*/, function(arg) { assert.ok(arg === 'success'); assertions++; } );
+    ee.on(/aaa.*/, function(arg) { assert.ok(arg === 'success'); assertions++; } );
+    ee.once(/aaa.*/, function(arg) { assert.ok(arg === 'success'); assertions++; done(); } );
     ee.emit('aaaa', 'success');
   },
 
-  'a when callback is only removed when it returns true': function(test) {
+  'a when callback is only removed when it returns true': function(done) {
     var items = [];
     ee.when(/aaa.*/, function(message) {
       items.push(message);
@@ -100,57 +108,56 @@ exports['given a new miniee'] = {
     ee.emit('aaaa', 3);
     ee.emit('aaaa', 4);
 
-    test.ok(items.some(function(message) {return message == 1;}));
-    test.ok(items.some(function(message) {return message == 2;}));
-    test.ok(items.some(function(message) {return message == 3;}));
-    test.ok(!items.some(function(message) {return message == 4;}));
-    test.equal(3, items.length);
-    test.done();
+    assert.ok(items.some(function(message) {return message == 1;}));
+    assert.ok(items.some(function(message) {return message == 2;}));
+    assert.ok(items.some(function(message) {return message == 3;}));
+    assert.ok(!items.some(function(message) {return message == 4;}));
+    assert.equal(3, items.length);
+    done();
   },
 
-  'can remove a single callback by string': function(test) {
-    test.expect(1);
-    var fail = function() { test.ok(false); };
+  'can remove a single callback by string': function(done) {
+    var fail = function() { assert.ok(false); };
     ee.on('tickets:21', fail);
     ee.once('tickets:21', fail);
     ee.removeListener('tickets:21', fail);
     ee.emit('tickets:21', 'data');
     setTimeout(function() {
-      test.ok(true);
-      test.done();
+      assert.ok(true);
+      done();
     }, 10);
   },
 
-  'can remove a single callback by regexp': function(test) {
-    var fail = function() { test.ok(false); };
+  'can remove a single callback by regexp': function(done) {
+    var fail = function() { assert.ok(false); };
     ee.on(new RegExp('^tickets:*'), fail);
     ee.once(new RegExp('^tickets:*'), fail);
     ee.removeListener(new RegExp('^tickets:*'), fail);
     ee.emit('tickets:21', 'data');
     setTimeout(function() {
-      test.done();
+      done();
     }, 10);
   },
 
-  'can remove all listeners from an event by string': function(test) {
-    var fail = function() { test.ok(false); };
+  'can remove all listeners from an event by string': function(done) {
+    var fail = function() { assert.ok(false); };
     ee.on('tickets:21', fail);
     ee.once('tickets:21', fail);
     ee.removeAllListeners('tickets:21');
     ee.emit('tickets:21', 'data');
     setTimeout(function() {
-      test.done();
+      done();
     }, 10);
   },
 
-  'can remove all listeners from an event by regexp': function(test) {
-    var fail = function() { test.ok(false); };
+  'can remove all listeners from an event by regexp': function(done) {
+    var fail = function() { assert.ok(false); };
     ee.on(new RegExp('^tickets:*'), fail);
     ee.once(new RegExp('^tickets:*'), fail);
     ee.removeAllListeners(new RegExp('^tickets:*'));
     ee.emit('tickets:21', 'data');
     setTimeout(function() {
-      test.done();
+      done();
     }, 10);
   }
 
@@ -159,6 +166,7 @@ exports['given a new miniee'] = {
 
 // if this module is the script being run, then run the tests:
 if (module == require.main) {
-  var nodeunit_runner = require('nodeunit-runner');
-  nodeunit_runner.run(__filename);
+  var mocha = require('child_process').spawn('mocha', [ '--colors', '--ui', 'exports', '--reporter', 'spec', __filename ]);
+  mocha.stdout.pipe(process.stdout);
+  mocha.stderr.pipe(process.stderr);
 }

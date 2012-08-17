2532986e8d694fc6746b9daa3487861982e60882
Mikito Takada <mikito.takada@gmail.com>
2012-08-17 12:03:04 -0700

    Fix jshint warnings

--- miniee.js
+++ miniee.js
@@ -1,8 +1,6 @@
 /**
  * MiniEE is a client and server side library for routing events.
  * Main difference from EventEmitter is that callbacks can be specified using RegExps.
- *
- * Should work on the client and the server.
  */
 
 var MiniEventEmitter	= function(){ };
@@ -98,7 +96,7 @@ MiniEventEmitter.prototype.removeAllListeners = function(expr) {
   }
   if(expr instanceof RegExp && this._routes) {
     this._routes = MiniEventEmitter.prototype.filter(this._routes, function(value) {
-      return !(value[0].toString() == expr.toString());
+      return value[0].toString() != expr.toString();
     });
   } else if(this._events && this._events[expr]) {
     this._events[expr] = [];
@@ -108,16 +106,16 @@ MiniEventEmitter.prototype.removeAllListeners = function(expr) {
 
 MiniEventEmitter.prototype.emit = function(event /* arg .. */) {
   // check all routes for a match
-  var args = Array.prototype.slice.call(arguments, 1);
+  var args = Array.prototype.slice.call(arguments, 1), i;
   if(this._events && this._events[event]) {
     // must run in reverse order to ensure that removing events won't skip callbacks
-    for(var i = this._events[event].length-1; i > -1 && this._events[event] && this._events[event][i]; i--){
+    for(i = this._events[event].length-1; i > -1 && this._events[event] && this._events[event][i]; i--){
       this._events[event][i].apply(this, args);
     }
   }
   if(this._routes) {
     // must run in reverse order to ensure that removing events won't skip callbacks
-    for(var i = this._routes.length-1; i > -1; i--){
+    for(i = this._routes.length-1; i > -1; i--){
       if(this._routes[i][0].test(event)) {
         this._routes[i][1].apply(this, args);
       }
@@ -126,9 +124,6 @@ MiniEventEmitter.prototype.emit = function(event /* arg .. */) {
   return this;
 };
 
-/**
- * mixin - augment the target object with the MiniEE functions
- */
 MiniEventEmitter.mixin	= function(destObject){
 	var props	= ['on', 'removeListener', 'removeAllListeners',  'emit', 'next', 'once', 'when'];
 	for(var i = 0; i < props.length; i ++){
